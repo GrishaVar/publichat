0a586d1444cbda1862a39086a9ffdeeb9fd9c176
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2022-04-09 00:58:57 +0200

    Changed arg types for consistency

--- src/msg.rs
+++ src/msg.rs
@@ -1,6 +1,6 @@
 use crate::constants::*;
 
-pub fn packet_to_storage(src: &[u8; MSG_IN_SIZE], dest: &mut [u8; MSG_ST_SIZE]) -> Hash {
+pub fn packet_to_storage(src: &MessageIn, dest: &mut MessageSt) -> Hash {
     // Takes bytes from client, 
     // Extract RSA pub, cypher, signature. Generate time.
     // Write buffer intended for storage into dest.
@@ -18,7 +18,7 @@ pub fn packet_to_storage(src: &[u8; MSG_IN_SIZE], dest: &mut [u8; MSG_ST_SIZE]) 
     chat_id
 }
 
-pub fn storage_to_packet(src: &[u8; MSG_ST_SIZE], dest: &mut [u8; MSG_OUT_SIZE], msg_id: u32) {
+pub fn storage_to_packet(src: &MessageSt, dest: &mut [u8], msg_id: u32) {
     // Takes bytes from chat file
     // adds the chat id and message id
     // Returns nothing but has side affects
