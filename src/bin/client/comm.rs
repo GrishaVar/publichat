acc6d8a5b4ff3f3d449de285182548daae8476eb
Grisha Vartanyan <grisha@vartanyan.com>
2022-08-03 17:46:27 +0200

    Update tui to prepare buffer paddings at compile-time
    - This is not an optimisation worth thinking too hard about
    - This should also eventually be done server-side
    - Update all client::comm sending functions to reduce index maths

--- src/bin/client/comm.rs
+++ src/bin/client/comm.rs
@@ -3,6 +3,24 @@ use std::net::TcpStream;
 use publichat::helpers::*;
 use publichat::constants::*;
 
+const fn pad_buf<const L: usize>(pad: [u8; PADDING_SIZE]) -> [u8; L] {
+    // Create an empty buffer, put pad in the beginning
+    // and END_PADDING at the end. This function is const!
+    // TODO: move this somewhere general? Do the same for server after tui merge
+    let mut buf = [0; L];
+    let mut i = 0;
+    while { i += 1; i <= PADDING_SIZE } {  // for-loops aren't const :/
+        let j = i - 1;  // TODO: find a way to avoid this?
+        buf[j] = pad[j];
+        buf[buf.len() - PADDING_SIZE + j] = END_PADDING[j];
+    }
+    buf
+}
+
+const EMPTY_MSG_BUF: [u8; MSG_IN_SIZE + 2*PADDING_SIZE] = pad_buf(SEND_PADDING);
+const EMPTY_FCH_BUF: [u8; FCH_SIZE + 2*PADDING_SIZE] = pad_buf(FETCH_PADDING);
+const EMPTY_QRY_BUF: [u8; QRY_SIZE + 2*PADDING_SIZE] = pad_buf(QUERY_PADDING);
+
 pub fn send_msg(  // TODO: create global "smrt" crate with en/decoding functions
     stream: &mut TcpStream,
     chat: &Hash,
@@ -10,22 +28,21 @@ pub fn send_msg(  // TODO: create global "smrt" crate with en/decoding functions
     signature: &Signature,
 ) -> Res {
     // TODO: rename constants to something direction-agnostic
-    let mut buf = [0; PADDING_SIZE + MSG_IN_SIZE + PADDING_SIZE];
+    let mut buf = EMPTY_MSG_BUF;
 
-    buf[..PADDING_SIZE].copy_from_slice(&SEND_PADDING);  // TODO: make padding const?
-    buf[PADDING_SIZE+MSG_IN_CHAT_ID..][..CHAT_ID_SIZE].copy_from_slice(chat);
-    buf[PADDING_SIZE+MSG_IN_CYPHER..][..CYPHER_SIZE].copy_from_slice(cypher);
-    buf[PADDING_SIZE+MSG_IN_SIGNATURE..][..SIGNATURE_SIZE].copy_from_slice(signature);
-    buf[PADDING_SIZE+MSG_IN_SIZE..][..PADDING_SIZE].copy_from_slice(&END_PADDING);
+    let cont = &mut buf[PADDING_SIZE..][..MSG_IN_SIZE];
+    cont[MSG_IN_CHAT_ID..][..CHAT_ID_SIZE].copy_from_slice(chat);
+    cont[MSG_IN_CYPHER..][..CYPHER_SIZE].copy_from_slice(cypher);
+    cont[MSG_IN_SIGNATURE..][..SIGNATURE_SIZE].copy_from_slice(signature);
 
     full_write(stream, &buf, "Failed to send message")
 }
 
 pub fn send_fetch(stream: &mut TcpStream, chat: &Hash) -> Res {
-    let mut buf = [0; PADDING_SIZE + FCH_SIZE + PADDING_SIZE];
-    buf[..PADDING_SIZE].copy_from_slice(&FETCH_PADDING);
-    buf[PADDING_SIZE+FCH_CHAT_ID..][..CHAT_ID_SIZE].copy_from_slice(chat);
-    buf[PADDING_SIZE+FCH_SIZE..][..PADDING_SIZE].copy_from_slice(&END_PADDING);
+    let mut buf = EMPTY_FCH_BUF;
+
+    let cont = &mut buf[PADDING_SIZE..][..FCH_SIZE];
+    cont[FCH_CHAT_ID..][..CHAT_ID_SIZE].copy_from_slice(chat);
 
     full_write(stream, &buf, "Failed to send fetch")
 }
@@ -38,13 +55,12 @@ pub fn send_query(
     id: u32,
 ) -> Res {
     if count > 0x7f || id > 0xffffff { return Err("Query input too large") }
+    let mut buf = EMPTY_QRY_BUF;
 
-    let mut buf = [0; PADDING_SIZE + QRY_SIZE + PADDING_SIZE];
-    buf[..PADDING_SIZE].copy_from_slice(&QUERY_PADDING);
-    buf[PADDING_SIZE+QRY_CHAT_ID..][..CHAT_ID_SIZE].copy_from_slice(chat);
-    buf[PADDING_SIZE+QRY_ARGS] = if forwards {count | 0x80} else {count};
-    buf[PADDING_SIZE+QRY_MSG_ID..][..MSG_ID_SIZE].copy_from_slice(&id.to_be_bytes()[1..]);
-    buf[PADDING_SIZE+QRY_SIZE..][..PADDING_SIZE].copy_from_slice(&END_PADDING);
+    let cont = &mut buf[PADDING_SIZE..][..QRY_SIZE];
+    cont[QRY_CHAT_ID..][..CHAT_ID_SIZE].copy_from_slice(chat);
+    cont[QRY_ARGS] = if forwards {count | 0x80} else {count};
+    cont[QRY_MSG_ID..][..MSG_ID_SIZE].copy_from_slice(&id.to_be_bytes()[1..]);
 
     full_write(stream, &buf, "Failed to send query")
 }
